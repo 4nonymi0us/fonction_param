320ddec0c5c62a4641908f10c641cd8daadb7399
4nonymi0us <87043252+4nonymi0us@users.noreply.github.com>
2022-11-24 12:18:45 +0100

    Add files via upload

---
+++ fonction_param.py
@@ -0,0 +1,18 @@
+#-*-coding:Latin-1 -*
+
+
+def winner(a, b):
+
+	if a>b:
+            
+           print("'a'est plus grand") 
+   elif a<b:
+            print("'b'est plus grand")
+
+   else: 
+
+   	         print("les deux valeurs sont égales")
+
+
+a=int(raw_input('entrez la valeur de "a":'))
+b=int(raw_input('entrez la valeurs de "b":'))
